46dc126f137cea7a7aa0331985c520ce08efa40f
Luxar <237206419+mary-uwadione@users.noreply.github.com>
2025-11-30 19:57:07 +0100

    add quiz file

--- quiz.js
+++ quiz.js
@@ -6,7 +6,7 @@ function checkAnswer() {
 if (userAnswer === correctAnswer) {
     feedback.textContent = "Correct! Well done.";
 } else {
-    feedback.textContent = "that's incorrect. Try again!";
+    feedback.textContent = "That's incorrect. Try again!";
 }
 }
 const submit = document.getElementById("submit-answer");
